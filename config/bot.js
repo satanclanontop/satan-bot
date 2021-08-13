e60fcb526f1bac91076d8a9cbfa771e72ae8a523
$atan <eryxsebeengone@gmail.com>
2021-08-13 19:13:41 -0400

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODc1ODY4ODMzNDEwMzUxMTc0.YRbyag.Hsgg8SPttWmb8QF5yvL2XMZ1a_k',
+        prefix: '-',
+        activity: '$tatus Unknown',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
