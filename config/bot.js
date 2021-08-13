7b1f1225047152f03dc8a8ad4f32a8a93bca6c2f
$atan <88906042+satanclanontop@users.noreply.github.com>
2021-08-13 18:39:34 -0400

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODc1ODY4ODMzNDEwMzUxMTc0.YRbyag.SQs5cvt6HA0eGv01cse29z4wzwE',
+        prefix: '-',
+        activity: '$tatus Unknown',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
